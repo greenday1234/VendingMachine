77ddd306561c4417c40e32f92c0874552729091e
Lee ChanHui <ing3616062@naver.com>
2024-05-24 22:11:13 +0900

    feat: 비밀번호 변경 창 뒤로가기 구현

--- VendingMachineClient/src/View/AdminPWChangeView.java
+++ VendingMachineClient/src/View/AdminPWChangeView.java
@@ -6,14 +6,12 @@ import validate.Validation;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 import static View.VendingMachineView.password;
 
 public class AdminPWChangeView {
 
-    private BufferedReader reader;
     private PrintWriter writer;
 
     public JPasswordField passwordChangeTextField;
@@ -23,13 +21,11 @@ public class AdminPWChangeView {
 
 
     public AdminPWChangeView(SocketDto socketDto) {
-
-        reader = socketDto.getReader();
         writer = socketDto.getWriter();
 
         // 프레임
         adminPWChangeFrame = new JFrame("비밀번호 변경");
-        adminPWChangeFrame.setBounds(560,200,500,600);
+        adminPWChangeFrame.setBounds(560,200,400,200);
         adminPWChangeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         adminPWChangeFrame.getContentPane().setLayout(new GridBagLayout());
         adminPWChangeFrame.getContentPane().setBackground(new Color(255, 255, 255, 255));
@@ -38,19 +34,31 @@ public class AdminPWChangeView {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(3, 3, 3, 3); // 여백 설정
 
-        addPasswordLabel(gbc, 0, 0);
-        addPasswordField(gbc, 1, 0);
-        addPasswordChangeLabel(gbc, 2, 0);
-        addPasswordChangeField(gbc, 3, 0);
-        addPasswordChangeButton(gbc, 4, 0);
+        addPasswordLabel(gbc, 1);
+        addPasswordField(gbc, 1);
+        addPasswordChangeLabel(gbc, 2);
+        addPasswordChangeField(gbc, 2);
+        addPasswordChangeButton(gbc, 2);
+
+        addBackButton(gbc, 0);
 
         adminPWChangeFrame.setVisible(true);
-
     }
 
-    private void addPasswordChangeButton(GridBagConstraints gbc, int row, int index) {
+    private void addBackButton(GridBagConstraints gbc, int row) {
+        JButton button = new JButton("창 닫기");
+        button.setPreferredSize(new Dimension(50, 40));
+        gbc.gridy = row;
+        gbc.anchor = GridBagConstraints.WEST;
+        button.addActionListener(e -> {
+            adminPWChangeFrame.setVisible(false);
+        });
+        adminPWChangeFrame.add(button, gbc);
+    }
+
+    private void addPasswordChangeButton(GridBagConstraints gbc, int row) {
         JButton button = new JButton("변경하기");
-        gbc.gridx = index;
+        button.setPreferredSize(new Dimension(100, 50));
         gbc.gridy = row;
         button.addActionListener(e -> {
             if (passwordTextField.getText().equals(password.getPassword())) {
@@ -69,36 +77,32 @@ public class AdminPWChangeView {
         adminPWChangeFrame.add(button, gbc);
     }
 
-    private void addPasswordChangeField(GridBagConstraints gbc, int row, int index) {
-        passwordChangeTextField = new JPasswordField("새로운 비밀번호를 입력해주세요.");
-        gbc.gridx = index;
+    private void addPasswordChangeField(GridBagConstraints gbc, int row) {
+        passwordChangeTextField = new JPasswordField("새로운 비밀번호 입력");
         gbc.gridy = row;
         adminPWChangeFrame.add(passwordChangeTextField, gbc);
     }
 
-    private void addPasswordChangeLabel(GridBagConstraints gbc, int row, int index) {
-        JLabel passwordChangeLabel = new JLabel("새로운 비밀번호를 입력해주세요.");
+    private void addPasswordChangeLabel(GridBagConstraints gbc, int row) {
+        JLabel passwordChangeLabel = new JLabel("새로운 비밀번호");
         Font font = passwordChangeLabel.getFont();
         passwordChangeLabel.setFont(font.deriveFont(Font.PLAIN, 15));
-        passwordChangeLabel.setPreferredSize(new Dimension(130, 50));
-        gbc.gridx = index;
+        passwordChangeLabel.setPreferredSize(new Dimension(150, 50));
         gbc.gridy = row;
         adminPWChangeFrame.add(passwordChangeLabel, gbc);
     }
 
-    private void addPasswordLabel(GridBagConstraints gbc, int row, int index) {
-        JLabel passwordLabel = new JLabel("사용 중이던 비밀번호를 입력해주세요.");
+    private void addPasswordLabel(GridBagConstraints gbc, int row) {
+        JLabel passwordLabel = new JLabel("기존 비밀번호");
         Font font = passwordLabel.getFont();
         passwordLabel.setFont(font.deriveFont(Font.PLAIN, 15));
-        passwordLabel.setPreferredSize(new Dimension(130, 50));
-        gbc.gridx = index;
+        passwordLabel.setPreferredSize(new Dimension(150, 50));
         gbc.gridy = row;
         adminPWChangeFrame.add(passwordLabel, gbc);
     }
 
-    private void addPasswordField(GridBagConstraints gbc, int row, int index) {
-        passwordTextField = new JPasswordField("사용 중이던 비밀번호를 입력해주세요.");
-        gbc.gridx = index;
+    private void addPasswordField(GridBagConstraints gbc, int row) {
+        passwordTextField = new JPasswordField("기존 비밀번호 입력");
         gbc.gridy = row;
         adminPWChangeFrame.add(passwordTextField, gbc);
     }
